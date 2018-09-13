1d06676fa415bda1fc68832dc289499bd233ffd7
Odin <odinuv@gmail.com>
2018-09-13 14:08:07 +0200

    fix: make the table larger

--- tests/Keboola/DbExtractor/RetryTest.php
+++ tests/Keboola/DbExtractor/RetryTest.php
@@ -54,7 +54,7 @@ class RetryTest extends AbstractMySQLTest
         $sourceFileName = $temp->getTmpFolder() . 'large.csv';
         $csv = new CsvFile($sourceFileName);
         $csv->writeRow(["usergender", "usercity", "usersentiment", "zipcode", "sku", "createdat", "category"]);
-        $rowCount = 200000;
+        $rowCount = 1000000;
         for ($i = 0; $i < $rowCount - 1; $i++) { // -1 for the header
             $csv->writeRow([uniqid('g'), "The Lakes", "1", "89124", "ZD111402", "2013-09-23 22:38:30", uniqid('c')]);
         }
