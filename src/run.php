379e27b6852196a70db28544e062c7e3de6a8f3b
David Střítežský <stritezskyd@gmail.com>
2018-10-17 13:08:01 +0200

    Clean up run.php

--- src/run.php
+++ src/run.php
@@ -4,36 +4,12 @@ declare(strict_types=1);
 
 use Keboola\Component\UserException;
 use Keboola\Component\Logger;
-use Symfony\Component\Serializer\Encoder\JsonDecode;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 require_once(dirname(__FILE__) . "/../vendor/autoload.php");
 
 $logger = new Logger();
 
 try {
-    $dataDir = getenv('KBC_DATADIR') ?? '/data';
-    $jsonDecode = new JsonDecode(true);
-
-    if (file_exists($dataDir . "/config.json")) {
-        $config = $jsonDecode->decode(
-            (string) file_get_contents($dataDir . '/config.json'),
-            JsonEncoder::FORMAT
-        );
-    } else {
-        throw new UserException('Configuration file not found.');
-    }
-
-    // get the state
-    $inputState = [];
-    $inputStateFile = $dataDir . '/in/state.json';
-    if (file_exists($inputStateFile)) {
-        $inputState = $jsonDecode->decode(
-            (string) file_get_contents($inputStateFile),
-            JsonEncoder::FORMAT
-        );
-    }
-
     $app = new \Keboola\MysqlExtractor\MysqlExtractor($logger);
     $app->run();
 
