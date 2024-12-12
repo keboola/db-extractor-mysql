81196c14b13ac5a8fa531d74728e3322014198fa
Marek Šuca <mark.suca@gmail.com>
2024-12-12 12:17:04 +0100

    at least container not fails at beginning

--- docker/mysql-entrypoint.sh
+++ docker/mysql-entrypoint.sh
@@ -3,5 +3,5 @@
 if [[ "$MYSQL_VERSION" =~ ^5\.6 ]]; then
   exec docker-entrypoint.sh mysqld --local-infile --port=3306 --default-authentication-plugin=mysql_native_password
 else
-  exec docker-entrypoint.sh mysqld --local-infile --port=3306 --mysql-native-password=ON
+  exec docker-entrypoint.sh mysqld --local-infile --port=3306 --upgrade=FORCE --caching-sha2-password-auto-generate-rsa-keys=ON
 fi
